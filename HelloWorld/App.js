e4581ca0b032c5bcf6df8ae63538ae8f053548d9
abhikumar18 <abhishek.it.msit@gmail.com>
2024-04-30 14:40:24 +0100

    Modal

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,29 +1,27 @@
-import { View,Text,Image,Button,Pressable } from "react-native";
-const logoImg = require("./assets/adaptive-icon.png")
+import { View,Text,Modal,Button } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
+import { useState } from "react";
 
 export default function App()
 {
+  const[isModalVisible,setIsModalVisible] = useState(false);
   return (
   <View style={{flex:1,backgroundColor:"plum",padding:60}}>
     <Button 
      title="Press" 
-     onPress={()=>alert("Button Pressed")} 
-     color="midnightblue"/>
-     <Pressable onPress={()=>console.log("Image Pressed")}>
-       <Image source={logoImg} style={{width:300,height:300}}/>
-     </Pressable>
-     <Pressable onPress={()=>console.log("Text Pressed")}>
-        <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-          Aldus PageMaker including versions of Lorem Ipsum.
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset.
-        </Text>
-        </Pressable>
+     onPress={()=>setIsModalVisible(true)} 
+     color="midnightblue"
+     />
+     <Modal visible={isModalVisible}>
+       <View style={{flex:1,backgroundColor:"lightblue",padding:60}}>
+       <Text>Modal Content</Text>
+       <Button 
+         title="Close" 
+         color="midnightblue"  
+         onPress={()=>setIsModalVisible(false)}
+         />
+       </View>
+     </Modal>
   </View>
   );
 }
