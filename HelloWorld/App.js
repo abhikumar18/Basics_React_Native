9943cbdcd3b14eb115f9f00701aebdb844fa75d7
abhikumar18 <abhishek.it.msit@gmail.com>
2024-05-01 12:18:24 +0100

    Stylesheet API

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,24 +1,16 @@
-import { View,Alert,Button } from "react-native";
-const logoImg = require("./assets/adaptive-icon.png");
+import {View,Text,StyleSheet} from 'react-native';
 
-export default function App()
+export default function App () 
 {
   return (
-  <View style={{flex:1,backgroundColor:"plum",padding:60}}>
-    <Button title="Alert 1" onPress={()=>Alert.alert("Invalid Data")} />
-    <Button title="Alert 2" onPress={()=>Alert.alert("Invalid Data","DOB is incorrect")} />
-    <Button 
-        title="Alert 3" 
-        onPress={()=>Alert.alert("Invalid Data","DOB is incorrect",[
-          {
-            text:"Cancel",
-            onPress:()=>console.log("Cancel Pressed"),
-          },
-          {
-            text:"OK",
-            onPress:()=>console.log("OK Pressed"),
-          }
-        ])} />
-  </View>
-  );
+    <View style={styles.container}>
+      <Text>
+        Stylesheet API
+      </Text>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container:{flex:1,backgroundColor:"plum",padding:60},
+})
