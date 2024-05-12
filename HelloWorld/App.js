748b793d120058e0b55b641d280a0851f6abb36e
abhikumar18 <abhishek.it.msit@gmail.com>
2024-05-12 11:29:38 +0100

    Fetching API data

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -2,73 +2,45 @@ import {
   View,
   StyleSheet,
   Text,
-  Button,
-  TextInput,
-  Image,
-  KeyboardAvoidingView,
-  Platform
+  StatusBar,
+  SafeAreaView,
+  FlatList
 } from 'react-native';
 import Box from './components/Box';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 export default function App () 
 {
-  const[username,setUsername] = useState("");
-  const[password,setPassword] = useState("");
-  const[errors,setErrors] = useState({});
+  const[postList,setPostList] = useState([]);
 
-  const validateForm = () => {
-    let errors ={}
-
-    if(!username) errors.username = "Username is required"
-    if(!password) errors.password = "Password is required"
-
-    setErrors(errors)
-    return Object.keys(errors).length === 0;
+  const fetchData = async(limit=10) => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
+      const data = await response.json();
+      setPostList(data);
   }
 
-  const handleSubmit = () => {
-    if(validateForm())
-      {
-        console.log("Submitted",username,password);
-        setUsername('');
-        setPassword('');
-        setErrors({});
-      }
-  }
 
+  useEffect(()=>{
+    fetchData();
+  },[]);
+  
   return (
-    <KeyboardAvoidingView 
-       style={styles.container} 
-       behavior="padding"
-       keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}
-       >
-       <View style={styles.form}>
-        <Image source={require('./assets/adaptive-icon.png')} style={styles.image} />
-          <Text style={styles.label}>Username</Text>
-          <TextInput 
-           style={styles.input}
-           placeholder='Enter your username'
-           value={username}
-           onChangeText={setUsername}
-          />
-          {
-          errors.username ? <Text style={styles.errorText}>{errors.username}</Text> : null
-          }
-          <Text>Password</Text>
-          <TextInput 
-           style={styles.input}
-           placeholder='Enter your password'
-           secureTextEntry
-           value={password}
-           onChangeText={setPassword}
-          />
-          {
-          errors.password ? <Text style={styles.errorText}>{errors.password}</Text> : null
-          }
-          <Button title='Login' onPress={handleSubmit} />
-       </View>
-    </KeyboardAvoidingView>
+      <SafeAreaView style={styles.container}>
+         <View style={styles.listContainer}>
+           <FlatList 
+           data={postList}
+           renderItem={({item})=>{
+            return (
+              <View style={styles.card}>
+                <Text style={styles.titleText}>{item.title}</Text>
+                <Text style={styles.bodyText}>{item.body}</Text>
+              </View>
+            )
+           }}
+           
+           />
+         </View>
+      </SafeAreaView>
       
   )
 }
@@ -76,45 +48,24 @@ export default function App ()
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    justifyContent:'center',
-    paddingHorizontal:20,
+    paddingTop:StatusBar.currentHeight,
     backgroundColor:"#f5f5f5"
   },
-  form:{
-    backgroundColor:'white',
-    padding:20,
-    borderRadius:10,
-    shadowColor:'black',
-    shadowOffset:{
-      width:0,
-      height:2,
-    },
-    shadowOpacity:0.25,
-    shadowRadius:4,
-    elevation:5,
+  listContainer:{
+    flex:1,
+    paddingHorizontal:16,
   },
-  label:{
-    fontSize:16,
-    marginBottom:5,
-    fontWeight:"bold",
+  card:{
+    backgroundColor:"white",
+    padding:16,
+    borderRadius:8,
+    borderWidth:1
   },
-  input:{
-    height:40,
-    borderColor:'#ddd',
-    borderWidth:1,
-    marginBottom:15,
-    padding:10,
-    borderRadius:5,
+  titleText:{
+    fontSize:30,
   },
-  image:{
-    width:200,
-    height:400,
-    alignSelf:"center",
-    marginBottom:50
+  bodyText:{
+    fontSize:24,
+    color:"#666666"
   },
-  errorText:{
-    color:"red",
-    marginBottom:10
-  }
-  
 });
